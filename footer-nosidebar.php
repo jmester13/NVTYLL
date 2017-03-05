85d1a8f524d1a7de42b801d56fe73172b9bf7afa
Joe E. Mester <jmester@andomain.auroranorthsoftware.com>
2017-03-05 15:15:23 -0500

    footer link to me update

--- footer-nosidebar.php
+++ footer-nosidebar.php
@@ -29,7 +29,8 @@
 
 				<?php dynamic_sidebar( 'footer-sidebar' ); ?>
 
-				<p class="col-sm-12 source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+				<p class="col-sm-12 source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> 
+				| Designed & Developed by <a href="http://joemester.com/" target="_blank"> Joe Mester </a> </p>
 
 			</div>
 
